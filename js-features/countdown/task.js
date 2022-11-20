886deb3b00336554c81d73847c53e8debb08081a
exceptr <98112445+exceptr@users.noreply.github.com>
2022-11-20 13:26:37 +0300

    complete

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,6 +1,7 @@
 const timer = document.getElementById('timer');
 let interval
- interval = setInterval(() => {
+
+interval = setInterval(() => {
     timer.textContent = Number(timer.textContent) - 1;
     if(timer.textContent == 0) {
         alert('Вы победили в конкурсе!');
