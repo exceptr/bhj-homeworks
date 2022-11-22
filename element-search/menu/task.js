e90285e5d40f6cc8bdd6b5b794c1b865586380d3
exceptr <98112445+exceptr@users.noreply.github.com>
2022-11-22 18:48:48 +0300

    fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,15 @@
-const menu_links = Array.from(document.querySelectorAll('.menu__link'));
-const menu_subs = Array.from(document.querySelectorAll('ul.menu_sub'));
+const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
 
-function closeMenu() {
-    for (let menu_sub_element of menu_subs) {
-        menu_sub_element.classList.remove('menu_active');
-    }
-}
-
-for (let menu_link of menu_links) {
-    menu_link.onclick = function () {
-        const parent = menu_link.parentElement;
-        closeMenu();
-        if (parent.querySelector('.menu_sub')) {
-            parent.querySelector('.menu_sub').classList.toggle('menu_active');
+for (let menuLink of menuLinks) {
+    menuLink.onclick = function () {
+        const parent = menuLink.parentElement;
+        if (parent.querySelector('.menu_sub').classList.contains('menu_active')) {
+            parent.querySelector('.menu_sub').classList.remove('menu_active');
+            return false;
         }
-        return false;
+        else if (parent.querySelector('.menu_sub')) {
+            parent.querySelector('.menu_sub').classList.toggle('menu_active');
+            return false;
+        }
     }
 }
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,16 +1,15 @@
-const modal__close_times = document.getElementsByClassName('modal__close_times');
-const modal_main = document.querySelectorAll('.modal');
-modal_main[0].className = "modal modal_active";
-const show_success = document.getElementsByClassName('show-success');
+const modalCloseTimes = Array.from(document.querySelectorAll('.modal__close_times'));
+const modalMain = document.querySelectorAll('.modal');
+modalMain[0].className = "modal modal_active";
+const showSucces = document.getElementsByClassName('show-success');
 
-modal__close_times[0].onclick = () => {
-    modal_main[0].className = "modal modal__close";
-}
-modal__close_times[1].onclick = () => {
-    modal_main[1].className = "modal modal__close";
+showSucces[0].onclick = () => {
+    modalMain[0].className = "modal";
+    modalMain[1].className = "modal modal_active";
 }
 
-show_success[0].onclick = () => {
-    modal_main[0].className = "modal";
-    modal_main[1].className = "modal modal_active";
+for (let modalClose of modalCloseTimes) {
+    modalClose.onclick = function() {
+        modalClose.closest('.modal').classList.remove('modal_active');
+    }
 }
