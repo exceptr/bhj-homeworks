94c923375adce1bacddde7aa846ece38c12ab35e
exceptr <98112445+exceptr@users.noreply.github.com>
2022-11-23 11:27:35 +0300

    fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,11 +3,7 @@ const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
 for (let menuLink of menuLinks) {
     menuLink.onclick = function () {
         const parent = menuLink.parentElement;
-        if (parent.querySelector('.menu_sub').classList.contains('menu_active')) {
-            parent.querySelector('.menu_sub').classList.remove('menu_active');
-            return false;
-        }
-        else if (parent.querySelector('.menu_sub')) {
+        if (parent.querySelector('.menu_sub')) {
             parent.querySelector('.menu_sub').classList.toggle('menu_active');
             return false;
         }
