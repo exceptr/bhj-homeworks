0aad8660e611a18ca18e99a75096903382cabda7
exceptr <98112445+exceptr@users.noreply.github.com>
2022-11-23 15:53:04 +0300

    Fix menu close

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,26 @@
-const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
+(function menu() {
+    const allLinks = Array.from(document.querySelectorAll('.menu__link'));
+    const menuLinks = allLinks.filter(item => item.nextElementSibling !== null);
 
-for (let menuLink of menuLinks) {
-    menuLink.onclick = function () {
-        const parent = menuLink.parentElement;
-        if (parent.querySelector('.menu_sub')) {
-            parent.querySelector('.menu_sub').classList.toggle('menu_active');
-            return false;
+    for (let menuLink of menuLinks) {
+        menuLink.onclick = function () {
+            if (menuLink.closest('.menu__item').querySelector('.menu_sub').classList.contains
+                    ('menu_active')) {
+                        menuLink.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active');
+                        return false
+                    }
+        else if (menuLink.closest('.menu__item').querySelector('.menu_sub')) {
+                closeMenu();
+                menuLink.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active');
+                return false;
+            }
         }
+    }
+})()
+
+function closeMenu() {
+    const menuSubs = Array.from(document.querySelectorAll('ul.menu_sub'));
+    for (let menuSuElement of menuSubs) {
+        menuSuElement.classList.remove('menu_active');
     }
 }
